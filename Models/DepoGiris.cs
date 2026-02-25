0bfb05a78fa823db5c18d1f645382e887e6382f7
LEVENT BAL <leventbal26@hotmail.com>
2026-02-25 15:24:01 +0300

    Initial commit

---
+++ Models/DepoGiris.cs
@@ -0,0 +1,18 @@
+namespace MagazaTakipApi.Models;
+
+public class DepoGiris
+{
+public int DepoGirisId { get; set; }            // PK
+public int UrunId { get; set; }                 // FK1 -> UrunId
+public int SatisHareketId { get; set; }     	// FK5 -> SatisHareketId
+public int FirmaId { get; set; }     		    // FK3 -> FirmaId
+
+public int İsyeriId { get; set; }     		    // FK2 -> İsyeriId
+
+public int PersonelId { get; set; }     	    // FK2 -> PersonelId
+
+public DateTime GelisTar { get; set; }
+
+
+	
+}
